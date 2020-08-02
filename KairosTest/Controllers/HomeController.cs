f5487ef80c0f24c6bde495f3d9f27a02a5641709
achmadzuhri <achmad.zzuhri@gmail.com>
2020-08-03 01:50:57 +0700

    init

---
+++ KairosTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KairosTest.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Minor()
+        {
+
+            return View();
+        }
+
+    }
+}
